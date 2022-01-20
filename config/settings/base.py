a53c73fbf6c617355d0c1dcf4e261692389119bd
mbronstein <mark@bronsteinlaw.com>
2022-01-20 14:04:34 -0500

    remove allauth

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,9 +66,6 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
-    "allauth",
-    "allauth.account",
-    "allauth.socialaccount",
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
@@ -105,7 +102,6 @@ MIGRATION_MODULES = {"sites": "ssa412.contrib.sites.migrations"}
 # https://docs.djangoproject.com/en/dev/ref/settings/#authentication-backends
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
